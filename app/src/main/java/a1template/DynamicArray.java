b98e47cb19ba5f712428b584132967bebaa777b9
Evelyn Ray <eray@smith.edu>
2025-02-14 00:46:01 -0500

    started

--- app/src/main/java/a1template/DynamicArray.java
+++ app/src/main/java/a1template/DynamicArray.java
@@ -5,9 +5,14 @@ package a1template;
 public class DynamicArray<T> implements IndexAccess<T>{
     
     // What instance variables do you need?
-
+    private T[] array;
+    private int high;
+    private int low;
     // Write a constructor to make a new DynamicArray Object from an array
+    public DynamicArray(int len){
+        this.array = Array.newInstance();
 
+    }
     // Now implement the methods from IndexAccess?
 
 }
